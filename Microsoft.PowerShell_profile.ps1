097651b807252ffc904bc3d72df83a72fd9f96dd
Maulana Sodiqin <maulanasdqn>
2022-10-30 07:37:54 +0700

    Maulana Sodiqin | Initial Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+. $env:USERPROFILE\.config\powershell\user_profile.ps1
+oh-my-posh init pwsh --config ~/catppuccin_frappe.omp.json | Invoke-Expression
---
+++ user_profile.ps1
@@ -0,0 +1,8 @@
+#Set Alias
+Set-Alias v vim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias c clear
+
+Import-Module posh-git
